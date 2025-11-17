3841d45ff21c1f143e6fec653560a091beee5d71
Md. Saddam Hossain Saykat <90493307+Saddamsaykat@users.noreply.github.com>
2025-11-18 01:21:50 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import routerBook from "./app/models/create-books/book.route";
+import { swaggerSpec } from "./app/config/swagger";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 
@@ -8,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", routerBook);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/", (req, res) => {
   res.send({
--- src/app/models/create-books/book.route.ts
+++ src/app/models/create-books/book.route.ts
@@ -3,6 +3,56 @@ import { bookController } from "./book.controller";
 
 const routerBook = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Books
+ *   description: Book management endpoints
+ */
+
+/**
+ * @swagger
+ * /create-book:
+ *   post:
+ *     summary: Create a new book
+ *     tags: [Books]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *           example:
+ *             title: "The Great Gatsby"
+ *             author: "F. Scott Fitzgerald"
+ *             price: 12.99
+ *             category: "Fiction"
+ *             description: "A classic American novel"
+ *             quantity: 50
+ *             inStock: true
+ *     responses:
+ *       201:
+ *         description: Book created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: "Book created successfully"
+ *                 data:
+ *                   $ref: '#/components/schemas/Book'
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
 routerBook.post("/create-book", bookController.createBook);
 
 export default routerBook;
