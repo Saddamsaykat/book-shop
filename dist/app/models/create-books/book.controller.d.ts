b73b9c346a12db08aa9d72f3ff48f96432fa9d86
Md. Saddam Hossain Saykat <90493307+Saddamsaykat@users.noreply.github.com>
2025-11-19 03:49:10 +0600

    Delete dist directory

--- dist/app/models/create-books/book.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-export declare const bookController: {
-    createBookController: (req: Request, res: Response) => Promise<void>;
-    getAllBooksController: (req: Request, res: Response) => Promise<void>;
-    getBookByIdController: (req: Request, res: Response) => Promise<void>;
-    updateBookByIdController: (req: Request, res: Response) => Promise<void>;
-    deleteBookByIdController: (req: Request, res: Response) => Promise<void>;
-};
-//# sourceMappingURL=book.controller.d.ts.map
